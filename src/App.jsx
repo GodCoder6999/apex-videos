4c154481f50816c75ba62a3a9ceb3db7a8582919
Anirban Bose <casuala459@gmail.com>
2026-03-31 01:59:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,14 @@ import Player from './pages/Player';
 
 function App() {
   return (
-    <div className="min-h-screen bg-primeBg text-white pb-10">
+    <div className="min-h-screen bg-[#0f171e] text-white pb-10">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
-        <Route path="/play/:type/:id" element={<Player />} />
         <Route path="/tv-shows" element={<TVShows />} />
-        {/* Dynamic route that accepts either 'movie' or 'tv' */}
         <Route path="/detail/:type/:id" element={<MovieDetail />} />
+        <Route path="/play/:type/:id" element={<Player />} />
       </Routes>
     </div>
   );
