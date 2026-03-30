129c18f715bcfed84bcffc79f446996fd18dc0da
Anirban Bose <casuala459@gmail.com>
2026-03-31 01:41:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import Movies from './pages/Movies';
 import TVShows from './pages/TVShows';
 import MovieDetail from './pages/MovieDetail';
+import Player from './pages/Player';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
+        <Route path="/play/:type/:id" element={<Player />} />
         <Route path="/tv-shows" element={<TVShows />} />
         {/* Dynamic route that accepts either 'movie' or 'tv' */}
         <Route path="/detail/:type/:id" element={<MovieDetail />} />
